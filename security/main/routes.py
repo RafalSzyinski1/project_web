7d59d76ca7e2c3a2c9d341f28eae82f366b6d352
Rafał Szyiński <77827305+RafalSzyinski1@users.noreply.github.com>
2023-05-07 15:12:43 +0200

    history of user and key

--- security/main/routes.py
+++ security/main/routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect
+from flask_login import login_required
 
 from security.models.models import KeyUsageHistory, Key, User
 
@@ -11,17 +12,19 @@ def about():
 
 
 @main.route("/history")
+@login_required
 def history():
+    page = request.args.get('page', 1, type=int)
     key_id = request.args.get('key', None, type=int)
     if key_id:
         key = Key.query.get_or_404(key_id)
         history = KeyUsageHistory.query.filter_by(key=key).order_by(
-            KeyUsageHistory.date.desc()).all()
-        return render_template("history.html", title=f"History of: {key.name}", history=history)
+            KeyUsageHistory.date.desc()).paginate(page=page, per_page=10)
+        return render_template("history.html", title=f"History of: {key.name}", history=history, key=key_id)
     user_id = request.args.get('user', None, type=int)
     if user_id:
         user = User.query.get_or_404(user_id)
         history = KeyUsageHistory.query.join(
-            Key).filter_by(key_user=user).all()
-        return render_template("history.html", title=f"History of: {user.name}", history=history)
+            Key).filter_by(key_user=user).order_by(KeyUsageHistory.date.desc()).paginate(page=page, per_page=10)
+        return render_template("history.html", title=f"History of: {user.name}", history=history, user=user_id)
     return redirect("/")
